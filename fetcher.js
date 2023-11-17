ccbd70687b6ed40c8edca90e0d3ed389d5b2ce7e
Faelixx <sasha.taylor7@gmail.com>
2023-11-17 06:40:40 -0700

    Feat: Add condition to check if local path is valid

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,7 @@
 /**
  * Takes two command line arguments
  * A URL and a local file path // node fetcher.js http://www.example.edu/ ./index.html
- * 
+ *
  * Make an HTTP request and wait for the response
  * After the http request is complete, take the data and write it to the file
  * Try using a promise function?
@@ -12,7 +12,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-})
+});
 
 const processArgs = process.argv;
 const url = processArgs[2];
@@ -20,6 +20,9 @@ const localPath = processArgs[3];
 
 
 request(url, (error, response, body) => {
+  if (!localPath) {
+    console.log('Invalid local path');
+  }
   if (!error && response.statusCode === 200) {
     fs.access(localPath, fs.constants.F_OK, (err) =>{
       console.log(`${localPath} ${err ? 'does not exist' : 'exists'}`);
@@ -27,7 +30,8 @@ request(url, (error, response, body) => {
         fs.writeFile(localPath, body, (err) => {
           if (err) throw err;
           console.log("Data has been written to the file successfully!");
-          console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`)
+          console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+          rl.close();
         });
       } else {
         rl.question("Would you like to overwrite file? (y/n)", (answer) => {
@@ -35,18 +39,18 @@ request(url, (error, response, body) => {
             fs.writeFile(localPath, body, (err) => {
               if (err) throw err;
               console.log("Data has been written to the file successfully!");
-              console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`)
+              console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
               rl.close();
             });
           } else {
             console.log("File unchanged.");
             rl.close();
           }
-        })
+        });
       }
-    })
+    });
   } else {
     console.log(`See response for details: ${response.statusCode}`);
     console.log(`An error has occured: ${error}`);
   }
-}); 
+});
